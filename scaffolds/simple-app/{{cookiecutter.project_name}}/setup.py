898df0116dc6bb94460fd0614a15f953554c3927
Josh Santos <josh@santos.cloud>
2024-07-17 22:25:18 +0700

    Improve output precision.

--- scaffolds/simple-app/{{cookiecutter.project_name}}/setup.py
+++ scaffolds/simple-app/{{cookiecutter.project_name}}/setup.py
@@ -8,7 +8,7 @@ def build_nuitka(build_func):
   if not BUILD_NUITKA:
       return
 
-  build_func.run_command('build_nuitka', {'build_dir': 'build', 'output_path': 'your_app_name'})
+  build_func.run_command('build_nuitka', {'build_dir': 'build', 'output_path': {{ cookiecutter.project_name }} })
 
 setup(
   name="{{ cookiecutter.project_name }}",
